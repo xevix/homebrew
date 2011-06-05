abd46bdb8f33f66975be526b4adf5a3cc5ed3fd5
Alejandro Wainzinger <aikawarazuni@gmail.com>
2011-06-05 16:54:51 -0700

    Update to use dschmidt's branch, and install with CMake.

--- Library/Formula/qtweetlib.rb
+++ Library/Formula/qtweetlib.rb
@@ -1,16 +1,20 @@
 require 'formula'
 
 class Qtweetlib < Formula
-  url 'git://github.com/minimoog/QTweetLib.git'
+  url 'git://github.com/dschmidt/QTweetLib.git'
   version 'trunk'
-  homepage 'https://github.com/minimoog/QTweetLib/'
+  homepage 'https://github.com/dschmidt/QTweetLib/'
+
+  depends_on 'cmake' => :build
   
   def install
+    args = std_cmake_parameters.split
+    args << ".."
+
     Dir.mkdir "build"
     Dir.chdir "build" do
-      system "qmake  .."
-      system "make sub-src"
-      prefix.install Dir['lib']
+      system "cmake", *args
+      system "make install"
     end
   end
 end
